13ccc8b2ca4b37e49dcd7b8cd0d6d26cd548e3a7
Edward Zhao <zhaoe@uw.edu>
2020-01-23 18:23:27 -0800

    preprocess mouse data

--- mouse_example.R
+++ mouse_example.R
@@ -1,10 +1,10 @@
 library(DropletUtils)
 library(scater)
-library(annotables)
 library(ggplot2)
 library(ggbeeswarm)
 library(tibble)
 library(AnnotationHub)
+library(scran)
 
 #Make sce
 sce_A2 = read10xCounts("data-raw/A2")
@@ -22,10 +22,29 @@ is.RBC = grep("^Hb", rownames(sce_A2))
 df_qc = perCellQCMetrics(sce_A2, subsets = list(Mito = is.mito,
                                           RBC = is.RBC))
 qc = quickPerCellQC(df_qc, percent_subsets = "subsets_Mito_percent")
-qc.lib = isOutlier(df_qc$sum, log=TRUE, type="lower")
-attr(qc.lib, "thresholds")
-qc.nexprs = isOutlier(df_qc$detected, log=TRUE, type="lower")
-attr(qc.nexprs, "thresholds")
-qc.mito = isOutlier(df_qc$subsets_Mito_percent, type="higher")
-attr(qc.mito, "thresholds")
-discard = qc.lib | qc.nexprs | qc.mito
+sce_A2$discard = qc$discard
+
+#Normalization
+clusters = quickCluster(sce_A2)
+sce_A2 = computeSumFactors(sce_A2, clusters = clusters)
+sce_A2 = logNormCounts(sce_A2)
+plot(librarySizeFactors(sce_A2), sizeFactors(sce_A2), pch=16,
+     xlab="Library size factors", ylab="Deconvolution factors", log="xy", 
+     col = ifelse(sce_A2$discard, "red", "black"))
+
+dec <- modelGeneVarByPoisson(sce_A2)
+top <- getTopHVGs(dec, prop=0.1)
+plot(dec$mean, dec$total, pch=16, cex=0.5,
+     xlab="Mean of log-expression", ylab="Variance of log-expression")
+curfit <- metadata(dec)
+curve(curfit$trend(x), col='dodgerblue', add=TRUE, lwd=2)
+
+#Dim reduction and clustering
+sce_A2 <- denoisePCA(sce_A2, technical=dec, subset.row=top)
+sce_A2 <- runTSNE(sce_A2, dimred="PCA")
+
+snn.gr <- buildSNNGraph(sce_A2, use.dimred="PCA", k=25)
+sce_A2$cluster <- factor(igraph::cluster_walktrap(snn.gr)$membership)
+table(sce_A2$cluster)
+plotTSNE(sce_A2, colour_by="cluster")
+
