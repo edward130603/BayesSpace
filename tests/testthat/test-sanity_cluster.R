1e38e47cb6bfea77f720325554a9f59b3485b4c0
Matt Stone <matthew.stone12@gmail.com>
2020-05-27 14:11:57 -0700

    add unit test to check refactored clustering against original results
    Former-commit-id: 52e6a23a18a1f82f12eeb107a2f21c84060fcc5e

---
+++ tests/testthat/test-sanity_cluster.R
@@ -0,0 +1,22 @@
+library(SingleCellExperiment)
+
+test_that("clustering matches", {
+  # sce <- readRDS("../../data/maynard_151673.rds")
+  sce <- readRDS("../../data/maynard_151673_subset.rds")
+  
+  positions = cbind(sce$imagecol, sce$imagerow)
+  colnames(positions) = c("x", "y") 
+  
+  set.seed(941)
+  out <- cluster(Y = metadata(sce)$PCs, 
+                 positions = as.matrix(positions), 
+                 q = 7, 
+                 init = colData(sce)$init_km7, 
+                 nrep = 1000, 
+                 gamma = 1.5, 
+                 dist = metadata(sce)$dist)
+  
+  labels <- apply(out$z[900:1000,], 2, Mode)
+  
+  expect_true(all(labels == colData(sce)$labels_q7_nrep1000))
+})
