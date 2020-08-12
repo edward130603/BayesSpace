fe95c211233dc6888c96a5c1ba4f83669567b25d
Matt Stone <mstone@fredhutch.org>
2020-08-12 14:33:13 -0700

    fast dmvnorm

--- src/cluster_refactor.cpp
+++ src/cluster_refactor.cpp
@@ -8,6 +8,47 @@
 using namespace Rcpp;
 using namespace arma;
 
+static double const log2pi = std::log(2.0 * M_PI);
+
+/* C++ version of the dtrmv BLAS function */
+void inplace_tri_mat_mult(arma::rowvec &x, arma::mat const &trimat){
+  arma::uword const n = trimat.n_cols;
+  
+  for(unsigned j = n; j-- > 0;){
+    double tmp(0.);
+    for(unsigned i = 0; i <= j; ++i)
+      tmp += trimat.at(i, j) * x[i];
+    x[j] = tmp;
+  }
+}
+
+// Borrowed with appreciation from Nino Hardt, Dicko Ahmadou, Ben Christofferson
+// https://gallery.rcpp.org/articles/dmvnorm_arma/
+arma::vec dmvnrm_arma_fast(arma::mat const &x,  
+                           arma::rowvec const &mean,  
+                           arma::mat const &sigma, 
+                           bool const logd=false) { 
+    using arma::uword;
+    uword const n = x.n_rows, 
+             xdim = x.n_cols;
+    arma::vec out(n);
+    arma::mat const rooti = arma::inv(trimatu(arma::chol(sigma)));
+    double const rootisum = arma::sum(log(rooti.diag())), 
+                constants = -(double)xdim/2.0 * log2pi, 
+              other_terms = rootisum + constants;
+    
+    arma::rowvec z;
+    for (uword i = 0; i < n; i++) {
+        z = (x.row(i) - mean);
+        inplace_tri_mat_mult(z, rooti);
+        out(i) = other_terms - 0.5 * arma::dot(z, z);     
+    }  
+      
+    if (logd)
+      return out;
+    return exp(out);
+}
+
 arma::mat update_mu(const arma::mat& Y, int q, const arma::rowvec& z_prev, 
                     const arma::vec& w, const arma::mat& lambda0, 
                     const arma::mat& lambda_prev, const arma::colvec& mu0vec) {
@@ -76,7 +117,7 @@ double compute_h_z(const arma::mat& df_sim_z, const arma::uvec& i_vector,
     pi_z = gamma / j_vector.size() * 2 * accu((df_sim_z(i_vector, j_vector) == z_j));
   }
   
-  double h_z = pi_z + dmvnorm(Y_j, vectorise(mu_k), sigma_i/w_j, true)[0];
+  double h_z = pi_z + dmvnrm_arma_fast(Y_j, mu_k, sigma_i/w_j, true)[0];
   
   return h_z;
 }
