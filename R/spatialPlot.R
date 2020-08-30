2e8a67dfda8f2404b56d996e5810a477b453d434
Matt Stone <matthew.stone12@gmail.com>
2020-08-29 17:36:13 -0700

    hex polygons for plotting Visium clusters

--- R/spatialPlot.R
+++ R/spatialPlot.R
@@ -28,20 +28,17 @@ palette <- c("#0173b2", "#de8f05", "#029e73", "#d55e00", "#cc78bc",
 #' @export
 #' @rdname spatialPlot
 clusterPlot <- function(sce, platform=c("Visium", "ST")) {
+    # TODO: add user-specified palette
     # TODO: add platform/lattice to sce metadata instead of passing
     platform <- match.arg(platform)
-    
+
     cdata <- data.frame(colData(sce))
-    message("Making spots")
     if (platform == "Visium") {
         vertices <- .make_hex_spots(cdata)
     } else {
         vertices <- .make_square_spots(cdata)
     }
-    
-    # TODO: redo with geom_polygon/square for ST
-    # TODO: redo with geom_polygon/hex so it scales and matches enhancePlot
-    message("plotting spots")
+
     splot <- ggplot(data=vertices, 
                     aes_(x=~x.vertex, y=~y.vertex, group=~spot, fill=~factor(fill))) + 
         geom_polygon() +
@@ -49,29 +46,74 @@ clusterPlot <- function(sce, platform=c("Visium", "ST")) {
         labs(fill="Cluster") +
         coord_equal() +
         theme_void()
-    
+
     splot
 }
 
-.make_hex_spots <- function(cdata) {
-   NULL
-}
-
-.make_square_spots <- function(cdata, fill.col="spatial.cluster") {
-    # Square spots are referenced by top left vertex
+.select_spot_positions <- function(cdata, fill.col="spatial.cluster") {
     spot_positions <- cdata[, c("col", "row", fill.col)]
     colnames(spot_positions) <- c("x.pos", "y.pos", "fill")
     spot_positions$spot <- rownames(spot_positions)
-    
-    vertex_offsets <- data.frame(x.offset=c(0, 1, 1, 0),
-                                  y.offset=c(0, 0, 1, 1))
-    
+
+    spot_positions
+}
+
+## Compute vertex coordinates for each spot
+## Positions are upper left corners for square and centers for hex
+.make_spot_vertices <- function(spot_positions, vertex_offsets) {
+    # Compute vertex coordinates in frame of plot
     spot_vertices <- merge(spot_positions, vertex_offsets)
-    
     spot_vertices$x.vertex <- spot_vertices$x.pos + spot_vertices$x.offset
     spot_vertices$y.vertex <- spot_vertices$y.pos + spot_vertices$y.offset
-    
+
     as.data.frame(spot_vertices)
+}
+
+## Hex spots are referenced by center
+## Spots are regular hexagons with one unit of horizontal distance
+## between centers
+.make_hex_spots <- function(cdata) {
+
+    spot_positions <- .select_spot_positions(cdata)
+
+    ## R = circumradius, distance from center to vertex
+    ## r = inradius, distance from center to edge midpoint
+    r <- 1/2
+    R <- (2 / sqrt(3)) * r
+
+    ## Start at (1-indexed origin)
+    spot_positions$x.pos <- spot_positions$x.pos - min(spot_positions$x.pos) + 1
+    spot_positions$y.pos <- spot_positions$y.pos - min(spot_positions$y.pos) + 1
+
+    ## Shift centers up so rows are adjacent
+    spot_positions$y.pos <- spot_positions$y.pos * R * (3/2)
+
+    ## Spot columns are offset by row
+    ## (i.e. odd rows have odd numbered columns, even rows have even)
+    ## Shift centers to the left so columns are adjacent (but hexes stay offset)
+    spot_positions$x.pos <- (spot_positions$x.pos + 1) / 2
+
+    ## vertices of each hex (with respect to center coordinates)
+    ## start at top center, loop clockwise
+    vertex_offsets <- data.frame(x.offset=c(0, r, r, 0, -r, -r),
+                                 y.offset=c(-R, -R/2, R/2, R, R/2, -R/2))
+
+    spot_vertices <- .make_spot_vertices(spot_positions, vertex_offsets)
+
+    ## Flip to match image orientation
+    spot_vertices$y.vertex <- -spot_vertices$y.vertex
+
+    spot_vertices
+}
+
+.make_square_spots <- function(cdata, fill.col="spatial.cluster") {
+    ## Square spots are referenced by top left vertex
+    vertex_offsets <- data.frame(x.offset=c(0, 1, 1, 0),
+                                  y.offset=c(0, 0, 1, 1))
+
+    spot_positions <- .select_spot_positions(cdata)
+
+    .make_spot_vertices(spot_positions, vertex_offsets)
 }
 
 #' @importFrom ggplot2 ggplot aes_ geom_polygon scale_fill_manual coord_fixed labs theme_void
@@ -84,7 +126,7 @@ enhancePlot <- function(sce.enhanced, sce.ref) {
 
     ## TODO store these as attributes when running spatialEnhance
     inputs <- .prepare_inputs(sce.ref)
-    
+
     positions_plot <- .polygon_positions(inputs$positions, inputs$xdist*1.01, 
         inputs$ydist*1.01, data.frame(sce.enhanced$spatial.cluster))
     colnames(positions_plot) <- c("x", "y", "group", "spatial.cluster")
@@ -116,17 +158,17 @@ enhancePlot <- function(sce.enhanced, sce.ref) {
                      1,2,7,
                      1,4,5),]
     group <- c(rep(seq(1, 6), each=3)) #six triangles
-    
+
     n0 <- nrow(positions)
     positions_long <- positions[rep(seq_len(n0), 18), ]
     shift2 <- t(t(shift2)*c(xdist, ydist))
     shift_long <- shift2[rep(seq_len(18), each=n0), ]
-    
+
     positions_long[,"x"] <- positions_long[,"x"] + shift_long[,"Var1"]
     positions_long[,"y"] <- positions_long[,"y"] + shift_long[,"Var2"]
     group_long <- paste(rep(seq_len(n0), 18),rep(rep(seq(1, 6), each=3), each=n0), sep="_")
     group_long2 <- data.frame(apply(str_split(group_long, "_", simplify=TRUE), 2, as.numeric))
-    
+
     #add columns
     colnames(group_long2) <- c("j0", "poly")
     cols <- as.matrix(cols)
